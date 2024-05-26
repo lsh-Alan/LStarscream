88c0f663e4d55c058fff238fcc6a54a13ecfb925
lsh-Alan <1273008040@qq.com>
2024-05-26 16:51:40 +0800

    commit project

---
+++ Tests/LStarscreamTests/LStarscreamTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import LStarscream
+
+final class LStarscreamTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        
+        
+    }
+}
